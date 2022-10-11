0d36eb07d2600cea0245297c15e1ab9ff046428f
Marco Moncada <marco.monkey93@gmail.com>
2022-10-11 15:05:52 +0200

    useGlobalPipes new ValidationPipe

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, ParseIntPipe, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthDto } from "./dto";// tutti i dettagli di questa cartella
 
@@ -11,7 +11,7 @@ export class AuthController {
     // signup(@Req() req: Request) {// @Req viene da ExpressJS
     signup(// utilizzando questo decoratore (@Body()) Express otterrà il corpo giusto senza preoccuparci del framework che utilizziamo
         //     @Body('email') email: string,
-        //     @Body('password') password: string
+        //     @Body('password', ParseIntPipe) password: string
         // ) {
         //     console.log({
         //         email,
@@ -21,7 +21,10 @@ export class AuthController {
         //     }) // anzichè fare una pipe per ogni campo di dati possiamo farne una per la forma DTO e applicare li le trasformazioni
         //semplifichiamo l'error handling con librerie 
         // di trasformatori di classe e validatori di classe
+
+
         @Body() dto: AuthDto) {
+        console.log({ dto })
         return this.authService.signup();
     }
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe(
+    //può eliminare ciò che non vogliamo venga inserito(dati indesiderati)   
+    //eliminando i dati che non sono definiti nel nostro DTO
+    { whitelist: true, }
+  ));
   await app.listen(3333);
 }
 bootstrap();
