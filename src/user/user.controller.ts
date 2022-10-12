dc523447806facfdbda24e61a66fd75620ffb96d
Marco Moncada <marco.monkey93@gmail.com>
2022-10-12 14:19:32 +0200

    create a custom guard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express'
+import { JwtGuard } from 'src/auth/guard';
 @Controller('users')
 export class UserController {//  http://localhost:3333/users/me
 
   // vogliamo bloccare questo percorso se non hai il gettone valido
-  @UseGuards(AuthGuard('jwt')) //questa route è protetta dalla Strategia JWT
+  @UseGuards(JwtGuard)
   @Get('me')
   getMe(@Req() req: Request) { // oggetto @Req viene da nest e l'interfaccia Req da Express
     // console.log({ user: req.user, })
