c55c5d6e759bc0139d518348a82c7d9dc2b6b84c
Marco Moncada <marco.monkey93@gmail.com>
2022-10-12 11:40:44 +0200

    create Access Token logic with JWT Service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,13 @@
 import { Module } from "@nestjs/common";
-import { PrismaModule } from "src/prisma/prisma.module";
+import { JwtModule } from "@nestjs/jwt";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 
 @Module({
-    imports: [PrismaModule],
-    controllers: [AuthController],
-    providers: [AuthService],
+	imports: [JwtModule.register({// per accedere e decodificare il token
+		// lo lasciamo bianco per personalizzarlo nel auth.service.ts
+	})],
+	controllers: [AuthController],
+	providers: [AuthService],
 })
 export class AuthModule { }
