3464fa1c83b4731f242ca6597cca132e9c88d6cb
Marco Moncada <marco.monkey93@gmail.com>
2022-10-12 12:12:40 +0200

    implement Strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,12 +2,13 @@ import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
+import { JwtStrategy } from "./strategy";
 
 @Module({
 	imports: [JwtModule.register({// per accedere e decodificare il token
 		// lo lasciamo bianco per personalizzarlo nel auth.service.ts
 	})],
 	controllers: [AuthController],
-	providers: [AuthService],
+	providers: [AuthService, JwtStrategy],
 })
 export class AuthModule { }
