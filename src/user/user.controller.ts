64e67e789da8736434ebee6cdd80d00ffba5ef12
Marco Moncada <marco.monkey93@gmail.com>
2022-10-14 15:25:52 +0200

    Create PATCH: edit user logic

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,18 +1,27 @@
-import { Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { GetUser } from '../auth/decorator';
 import { JwtGuard } from '../auth/guard';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
 
 // vogliamo bloccare questo percorso se non hai il gettone valido
 // tutto quello che è in user.controller richiede di fornire un token
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {//  http://localhost:3333/users/me
+  constructor(private userService: UserService) { }
   @Get('me')
   getMe(@GetUser() user: User) { // vogliamo passare solo l'id dell'oggetto user
     // console.log({ user: req.user, })
     return user;
   }
+
   @Patch()
-  editUser() { }
+  editUser(
+    @GetUser('id') userId: number,
+    @Body() dto: EditUserDto
+  ) {
+    return this.userService.editUser(userId, dto);
+  }
 }
